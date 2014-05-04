b7b29b40e6cfb48177a9a504b9bba2f1aedf53d3
isaac <isaac@Kratos-Pc>
2014-05-04 03:34:23 +0200

    IPosition  Position added
    generatePosition is not my method

--- Sokoban_3/src/sokoban/IPosition.java
+++ Sokoban_3/src/sokoban/IPosition.java
@@ -5,4 +5,16 @@ public interface IPosition {
 
 	// Methods that a position should have.
 	public Position generatePosition(Figure figure);
+	
+	public void setLocation(double x,double y); 
+	public double getX();
+	public void setX(double x);
+	public double getY();
+	public void setY(double y);	
+	public boolean isEqualto(Figure f);
+	public IPosition Clone();
+	
+	
+	
+
 }
--- Sokoban_3/src/sokoban/Position.java
+++ Sokoban_3/src/sokoban/Position.java
@@ -4,14 +4,66 @@ package sokoban;
 // Class that represents the position of a figure as coordinates in table
 public class Position implements IPosition {
 
-	public Position(){
+	 private double x =0;
+	 private double y =0;
+	 
+	  
+
+	public double getX() {
+		return this.x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return this.y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+	public void setLocation(double x, double y) {
+		this.x = x;
+		this.y = y;
 		
 	}
 	
+	public boolean isEqualto(Figure f) {
+		if(f.getX()== this.x && f.getY()== this.y){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public IPosition Clone(){		
+		IPosition p = new Position();
+		p.setLocation(this.x, this.y);		
+		return p;		
+	}
+	
+	public Position(){		
+			this.x = 0;
+			this.y = 0;				
+		}
+	
+	public Position(IPosition p){		
+		this.setLocation(p.getX(), p.getY());			
+	}
+	
+	
+	
+	
 	@Override
 	public Position generatePosition(Figure figure) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub	
 		return null;
 	}
+
+	 
+	
+	
 
 }
